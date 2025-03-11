b9593b43605c0124cbcc0dc2ceca6794058eeeb0
tamilselvibalu <victorphil25@gmail.com>
2025-03-11 11:39:34 +0000

    console log

--- skills.js
+++ skills.js
@@ -1,7 +1,6 @@
-function calculateNumbers (var1, var2) {
-  return var1 + var2;
-}
-
 function calculateNumbers(var1, var2) {
-  return var1 - var2;
+  return var1 + var2;
+const result = calculateNumbers(var1, var2);
+console.log('Result of the calculation is: ' + result);
+
 }
